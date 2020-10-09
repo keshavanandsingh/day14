98eb9a18fe54b98ead07a9954377b80ff087032a
keshavanandsingh <keshavanand22@gmail.com>
2020-10-10 01:17:15 +0530

    added

--- src/main/java/Binary/Search/Tree/BinarySearchTree.java
+++ src/main/java/Binary/Search/Tree/BinarySearchTree.java
@@ -1,5 +1,38 @@
 package Binary.Search.Tree;
 
-public class BinarySearchTree<T> {
+public class BinarySearchTree<K extends Comparable<K>> {
+	private BinarySearchTreeNode<K> rootNode;
 
+	public void add(K key) {
+		this.rootNode = this.addNodeRecursively(rootNode, key);
+
+	}
+
+	private BinarySearchTreeNode<K> addNodeRecursively(BinarySearchTreeNode<K> currentNode, K key) {
+		if (currentNode == null)
+			return new BinarySearchTreeNode<>(key);
+
+		if (key.compareTo(currentNode.key) == 0)
+			return currentNode;
+
+		if (key.compareTo(currentNode.key) < 0) {
+			currentNode.leftNode = addNodeRecursively(currentNode.leftNode, key);
+		}
+
+		else {
+			currentNode.rightNode = addNodeRecursively(currentNode.rightNode, key);
+		}
+		return currentNode;
+	}
+
+	public int getSize() {
+
+		return this.getSizeRecursively(rootNode);
+	}
+
+	private int getSizeRecursively(BinarySearchTreeNode<K> currentNode) {
+
+		return (currentNode == null) ? 0
+				: 1 + this.getSizeRecursively(currentNode.leftNode) + this.getSizeRecursively(currentNode.rightNode);
+	}
 }
--- src/test/java/Binary/Search/Tree/BinarySearchTreeTest.java
+++ src/test/java/Binary/Search/Tree/BinarySearchTreeTest.java
@@ -4,17 +4,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BinarySearchTreeTest {
-
 	@Test
-	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSize3() {
+	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSize3() {
 		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
 		binarySearchTree.add(56);
 		binarySearchTree.add(30);
 		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(11);
+		binarySearchTree.add(65);
+		binarySearchTree.add(3);
+		binarySearchTree.add(16);
+		binarySearchTree.add(63);
+		binarySearchTree.add(67);
 
 		int treeSize = binarySearchTree.getSize();
-		System.out.println("tree size is "+treeSize);
+		System.out.println("tree size is " + treeSize);
 
-		Assert.assertEquals(3, treeSize);
+		Assert.assertEquals(13, treeSize);
 	}
 }
