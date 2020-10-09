a4d6816d029b97843cba0dd1d0663676c1060579
keshavanandsingh <keshavanand22@gmail.com>
2020-10-10 01:22:16 +0530

    added

--- src/main/java/Binary/Search/Tree/BinarySearchTree.java
+++ src/main/java/Binary/Search/Tree/BinarySearchTree.java
@@ -35,4 +35,27 @@ public class BinarySearchTree<K extends Comparable<K>> {
 		return (currentNode == null) ? 0
 				: 1 + this.getSizeRecursively(currentNode.leftNode) + this.getSizeRecursively(currentNode.rightNode);
 	}
+
+	public boolean search(K key) {
+		return (searchRecursively(rootNode, key) != null) ? true : false;
+	}
+
+	private BinarySearchTreeNode<K> searchRecursively(BinarySearchTreeNode<K> currentNode, K key) {
+
+		if (currentNode == null)
+			return null;
+		else {
+			if (key.compareTo(currentNode.key) == 0)
+				return currentNode;
+
+			if (key.compareTo(currentNode.key) < 0) {
+				return searchRecursively(currentNode.leftNode, key);
+			}
+
+			else {
+				return searchRecursively(currentNode.rightNode, key);
+			}
+		}
+
+	}
 }
--- src/test/java/Binary/Search/Tree/BinarySearchTreeTest.java
+++ src/test/java/Binary/Search/Tree/BinarySearchTreeTest.java
@@ -5,7 +5,20 @@ import org.junit.Test;
 
 public class BinarySearchTreeTest {
 	@Test
-	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSize3() {
+	public void given3NumbersWhenAddedToBinaryTreeShouldReturnSize3() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+
+		int treeSize = binarySearchTree.getSize();
+		System.out.println("tree size is " + treeSize);
+
+		Assert.assertEquals(3, treeSize);
+	}
+
+	@Test
+	public void given13NumbersWhenAddedToBinaryTreeShouldReturnSize13() {
 		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
 		binarySearchTree.add(56);
 		binarySearchTree.add(30);
@@ -25,5 +38,27 @@ public class BinarySearchTreeTest {
 		System.out.println("tree size is " + treeSize);
 
 		Assert.assertEquals(13, treeSize);
+	}
+
+	@Test
+	public void givenANumberWhenSearchedInBinaryTreeShouldReturnTrue() {
+		BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
+		binarySearchTree.add(56);
+		binarySearchTree.add(30);
+		binarySearchTree.add(70);
+		binarySearchTree.add(22);
+		binarySearchTree.add(40);
+		binarySearchTree.add(60);
+		binarySearchTree.add(95);
+		binarySearchTree.add(11);
+		binarySearchTree.add(65);
+		binarySearchTree.add(3);
+		binarySearchTree.add(16);
+		binarySearchTree.add(63);
+		binarySearchTree.add(67);
+
+		boolean searchNode = binarySearchTree.search(63);
+
+		Assert.assertTrue(searchNode);
 	}
 }
